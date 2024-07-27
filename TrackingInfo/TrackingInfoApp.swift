a48c280419112f7140d310163ff6a1eedde1a3de
ammar <muhammad.ammar@appiskey.com>
2024-07-27 15:58:54 +0500

    Initial Commit

---
+++ TrackingInfo/TrackingInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingInfoApp.swift
+//  TrackingInfo
+//
+//  Created by mac on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackingInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
